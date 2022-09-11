96661cf92e8cce9cb2ca095ad5c1d0d8c8f1e1f5
BoBkiNN_ <bobkin.yangirov@gmail.com>
2022-09-11 18:22:46 +0600

    update

--- src/main/java/xyz/bobkinn_/dimplaceholders/ReloadCmd.java
+++ src/main/java/xyz/bobkinn_/dimplaceholders/ReloadCmd.java
@@ -4,11 +4,12 @@ import net.md_5.bungee.api.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 
 public class ReloadCmd implements CommandExecutor{
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
         Main.reload();
         String rlMsg = Main.config.getString("reloadMsg","&aReloaded!");
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', rlMsg));
